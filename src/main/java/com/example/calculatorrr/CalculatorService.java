06b084f1813f41704c4a0d817f5b1353d81cbe2d
Redyx <dr.redyx@gmail.com>
2021-12-05 23:16:04 +0300

    NEW

--- src/main/java/com/example/calculatorrr/CalculatorService.java
+++ src/main/java/com/example/calculatorrr/CalculatorService.java
@@ -1,4 +1,31 @@
 package com.example.calculatorrr;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    public int sum(Integer number1, Integer number2) {
+        return number1 + number2;
+    }
+
+    public int decrease(Integer number1, Integer number2) {
+        return number1 - number2;
+    }
+
+    public int multiplication(Integer number1, Integer number2) {
+        return number1 * number2;
+    }
+
+    public int division(Integer number1, Integer number2) {
+        return number1/number2;
+    }
+
+    public boolean flag(Integer number1, Integer number2) {
+        if (number1 == null || number2 == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
